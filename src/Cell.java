9d29c51fd3877be4c0839512ab65cf14d6aeb404
James Yanyuk <jyanyuk@gmail.com>
2014-12-16 17:23:27 -0500

    Added drawing mode, finished implementing rule-set

--- src/Cell.java
+++ src/Cell.java
@@ -21,34 +21,34 @@ public class Cell {
         alive = false;
     }
 
-    public void pulseCell(Cell[][] grid, Stack<Cell> toNeutralize, Stack<Cell> toRevitalize) {
-        checkNeighbors(grid, toNeutralize, toRevitalize);
+    public void pulseCell(Cell[][] grid, Stack<Cell> toNeutralize, Stack<Cell> toRevitalize, int xBound, int yBound) {
+        checkNeighbors(grid, toNeutralize, toRevitalize, xBound, yBound);
     }
 
-    private void checkNeighbors(Cell[][] grid, Stack<Cell> toNeutralize, Stack<Cell> toRevitalize) {
-        if(alive == true && neighbors(grid) != 2 && neighbors(grid) != 3) {
+    private void checkNeighbors(Cell[][] grid, Stack<Cell> toNeutralize, Stack<Cell> toRevitalize, int xBound, int yBound) {
+        if(alive == true && neighbors(grid, xBound, yBound) != 2 && neighbors(grid, xBound, yBound) != 3) {
             toNeutralize.push(this);
             //System.out.println(x + ", " + y + " not enough neighbors " + neighbors(grid));
-        } else if(alive == false && neighbors(grid) == 3) {
+        } else if(alive == false && neighbors(grid, xBound, yBound) == 3) {
             toRevitalize.push(this);
         }
     }
 
-    private int neighbors(Cell[][] grid) {
+    private int neighbors(Cell[][] grid, int xBound, int yBound) {
         // Check 8 surrounding cells for live neighbors
         int neighbors = 0;
 
         neighbors += (x - 1) >= 0 && grid[x - 1][y].alive ? 1 : 0;
-        neighbors += (x + 1) < 60 && grid[x + 1][y].alive ? 1 : 0;
+        neighbors += (x + 1) < xBound && grid[x + 1][y].alive ? 1 : 0;
 
         neighbors += (y - 1) >= 0 && grid[x][y - 1].alive ? 1 : 0;
-        neighbors += (y + 1) < 60 && grid[x][y + 1].alive ? 1 : 0;
+        neighbors += (y + 1) < yBound && grid[x][y + 1].alive ? 1 : 0;
 
         neighbors += (x - 1) >= 0 && (y - 1) >= 0 && grid[x - 1][y - 1].alive ? 1 : 0;
-        neighbors += (x + 1) < 60 && (y + 1) < 60 && grid[x + 1][y + 1].alive ? 1 : 0;
+        neighbors += (x + 1) < xBound && (y + 1) < yBound && grid[x + 1][y + 1].alive ? 1 : 0;
 
-        neighbors += (x - 1) >= 0 && (y + 1) < 60 && grid[x - 1][y + 1].alive ? 1 : 0;
-        neighbors += (x + 1) < 60 && (y - 1) >= 0 && grid[x + 1][y - 1].alive ? 1 : 0;
+        neighbors += (x - 1) >= 0 && (y + 1) < yBound && grid[x - 1][y + 1].alive ? 1 : 0;
+        neighbors += (x + 1) < xBound && (y - 1) >= 0 && grid[x + 1][y - 1].alive ? 1 : 0;
 
         return neighbors;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,47 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Input;
 
 import java.util.*;
 
 public class Main extends BasicGame {
 
-    private static int gridX = 60;
-    private static int gridY = 60;
-    private static int gridElemSize = 10;
+    private static int gridX = 200;
+    private static int gridY = 140;
+    private static int gridElemSize = 5;
+
+    private static final int MODE_DRAW = 0;
+    private static final int MODE_SIMULATE = 1;
+    private static int mode = MODE_DRAW;
 
     Cell[][] grid = new Cell[gridX][gridY];
 
     public Stack<Cell> toNeutralize = new Stack();
     public Stack<Cell> toRevitalize = new Stack();
 
+    private Input input;
+
+    private int runningTime = 0;
+    private int updateTime = 30;
+
     public Main(String title) {
         super(title);
     }
 
     private void initGrid() {
-        for(int r = 0; r < gridY; r++) {
-            for(int c = 0; c < gridX; c++) {
+        for(int r = 0; r < gridX; r++) {
+            for(int c = 0; c < gridY; c++) {
                 grid[r][c] = new Cell(r, c);
             }
         }
     }
 
-    public void pulseGrid() {
+    private void pulseGrid() {
         // Update board by iterating through stacks
         // (neutralize and revitalize stacks)
 
-        System.out.println("Pulsing grid...");
+        //System.out.println("Pulsing grid...");
 
         while(!toNeutralize.isEmpty()) {
             toNeutralize.pop().neutralize();
@@ -45,48 +55,80 @@ public class Main extends BasicGame {
         }
     }
 
+    private void catchInput(GameContainer gc, int delta) {
+        if(input.isKeyPressed(Input.KEY_P)) {
+            runningTime = 0;
+
+            if(mode == MODE_DRAW) mode = MODE_SIMULATE;
+            else if(mode == MODE_SIMULATE) mode = MODE_DRAW;
+        }
+    }
+
+    private void placeCell(double x, double y) {
+        int xPos = (int) x / gridElemSize;
+        int yPos = (int) y / gridElemSize;
+
+        grid[xPos][yPos].revitalize();
+    }
+
+    private void removeCell(double x, double y) {
+        int xPos = (int) x / gridElemSize;
+        int yPos = (int) y / gridElemSize;
+
+        grid[xPos][yPos].neutralize();
+    }
+
     public void init(GameContainer gc) throws SlickException {
-        gc.setMinimumLogicUpdateInterval(500);
+        input = gc.getInput();
+
+        //gc.setMinimumLogicUpdateInterval(500);
         gc.setTargetFrameRate(60);
         gc.setAlwaysRender(true);
         gc.setVSync(true);
 
         initGrid();
-
-        /*
-         * Generate sample initial configuration
-         * Should form basic pulsar
-        */
-        grid[29][30].revitalize();
-        grid[30][30].revitalize();
-        grid[31][30].revitalize();
     }
 
     public void render(GameContainer gc, Graphics g) throws SlickException {
-        g.drawString(Integer.toString(gc.getFPS()), 0, 0);
-
         // Set background color to white
         g.setBackground(Color.white);
+
+        g.setColor(Color.black);
+
+        g.drawString("Cellular Automaton - Conway's Game of Life", 5, 0);
+        g.drawString("Press 'P' to toggle drawing mode (default)", 5, 20);
+        g.drawString("Mode: " + (mode == MODE_DRAW ? "Draw" : "Simulation"), 5, gridY * gridElemSize - 20);
 
         // Draw cell grid
         for(Cell[] r : grid) {
             for (Cell c : r) {
-                if (c.alive) {
-                    g.setColor(Color.black);
+                if (c.alive)
                     g.fillRect(c.x * gridElemSize, c.y * gridElemSize, gridElemSize, gridElemSize);
-                }
             }
         }
     }
 
     public void update(GameContainer gc, int delta) throws SlickException {
-        for(Cell[] r : grid) {
-            for (Cell c : r) {
-                c.pulseCell(grid, toNeutralize, toRevitalize);
-            }
-        }
+        // Catch any key or mouse input
+        catchInput(gc, delta);
 
-        pulseGrid();
+        runningTime += delta;
+
+        if (mode == MODE_SIMULATE && runningTime >= updateTime) {
+            for (Cell[] r : grid) {
+                for (Cell c : r) {
+                    c.pulseCell(grid, toNeutralize, toRevitalize, gridX, gridY);
+                }
+            }
+
+            pulseGrid();
+            runningTime = 0;
+        } else if (mode == MODE_DRAW) {
+            if (input.isMouseButtonDown(0))
+                placeCell(input.getMouseX(), input.getMouseY());
+            if (input.isMouseButtonDown(1))
+                removeCell(input.getMouseX(), input.getMouseY());
+        }
     }
 
     public static void main(String[] args) {
